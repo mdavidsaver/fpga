470ef9fbeb83c8289c0ef21dc76560c6bdbc2038
Michael Davidsaver <mdavidsaver@gmail.com>
2016-04-23 17:00:59 -0400

    uart rx filtering

---
+++ modules/uart/uart_rx_filter.v
@@ -0,0 +1,33 @@
+module uart_rx_filter(
+  input wire samp_clk,
+  input wire in,
+  input wire bit_clk,
+  output wire out
+);
+
+reg [1:0] sync; // synchronize async input with shift register
+wire in_sync = sync[0];
+
+reg [1:0] cnt = 0; // counting deadband filter to de-bounce
+
+always @(posedge samp_clk)
+  if(bit_clk)
+  begin
+    sync <= {in, sync[1]};
+
+    if(in_sync==1 && cnt!=2'b11)
+      cnt <= cnt+1;
+    else if(in_sync==0 && cnt!=2'b00)
+      cnt <= cnt-1;
+    else
+      cnt <= cnt;
+
+    if(cnt==2'b11)
+      out <= 1;
+    else if(cnt==2'b00)
+      out <= 0;
+    else
+      out <= out;
+  end
+
+endmodule
