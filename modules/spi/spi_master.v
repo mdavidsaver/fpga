0528183f75392414bb62f0ffe4802ec8eeb105ba
Michael Davidsaver <mdavidsaver@gmail.com>
2016-06-19 21:02:44 -0400

    SPI master add cpol and cphas controls
    support all 4 clock modes

--- modules/spi/spi_master.v
+++ modules/spi/spi_master.v
@@ -1,14 +1,20 @@
 module spi_master(
   input  wire       clk4,  // 4x the SPI bus clock
 
+  input  wire       cpol,  // clock polarity (idle level)
+  input  wire       cpha,  // clock phase. 0 - sample on rising edge,
+                           //              1 - sample on falling edge
+
   output reg        mclk,  // clk4/4
   output reg        mosi,
   input  wire       miso,
 
-  input  wire [7:0] din,
-  output reg  [7:0] dout,
-  input  wire       start,
-  output wire       busy
+  input  wire [7:0] din,   // data to be sent by master
+  output reg  [7:0] dout,  // data received by master
+  input  wire       start, // toggle high to start transfer
+  output wire       busy   // high while transfer in progress
+                           // rising edge when 'start' toggled,
+                           // falling edge when transfer complete
   
 );
 
@@ -23,28 +29,46 @@ reg [3:0] cnt = 0;
 assign busy = cnt!=0;
 
 always @(posedge clk4)
-  if(!busy && start) begin
-    dout <= din;
-    cnt    <= 8;
-  end else if(!busy) begin
-    mclk <= 0;
+  if(!busy) begin
+    mclk <= cpol;
+    mosi <= 0;
+    if(start) begin
+      dout <= din;
+      cnt  <= 8;
+    end else begin
 `ifdef SIM
-    dout <= 8'hxx;
+      dout <= 8'hxx;
 `else
-    dout <= 0;
+      dout <= 0;
 `endif
-  end
-
-always @(posedge clk4)
-  if(busy) begin
-    // CPOL=0, CPHA=1
+    end
+  end else begin
+    // CPHA=0
     case(phas)
-    0: mosi   <= dout[7];
-    1: mclk   <= 1;
-    2: dout <= {dout[6:0], miso};
+    // setup
+    0: begin
+       mclk   <= cpol;
+       mosi   <= dout[7];
+       end
+    // tick, slave may sample
+    1: begin
+       mclk   <= ~cpol;
+       mosi   <= dout[7];
+       if(cpha==0)
+         dout   <= {dout[6:0], miso};
+       end
+    // master sample, slave must sample by this point
+    2: begin
+       mclk   <= ~cpol;
+       mosi   <= dout[7];
+       end
+    // tock
     3:begin
-       mclk   <= 0;
+       mclk   <= cpol;
+       mosi   <= dout[7];
        cnt    <= cnt-1;
+       if(cpha==1)
+         dout   <= {dout[6:0], miso};
       end
     endcase
   end
--- tests/spi_master_tb.v
+++ tests/spi_master_tb.v
@@ -2,23 +2,26 @@ module test;
 
 `include "utest.vlib"
 
-`TEST_PRELUDE(6)
+`TEST_PRELUDE(24)
 
 `TEST_CLOCK(clk,1);
 
-`TEST_TIMEOUT(200)
+`TEST_TIMEOUT(2000)
 
 wire mclk, mosi;
-reg miso;
+reg miso = 1'bz;
 
 wire busy;
-reg start;
+reg start, cpol = 0, cpha = 0;
 wire [7:0] dout;
 reg [7:0] din;
 reg [7:0] dshift;
 
 spi_master D(
   .clk4(clk),
+
+  .cpol(cpol),
+  .cpha(cpha),
 
   .mclk(mclk),
   .mosi(mosi),
@@ -30,6 +33,8 @@ spi_master D(
   .busy(busy)
 );
 
+integer i;
+
 task spi_shift;
   input [7:0] mval;
   input [7:0] sval;
@@ -40,48 +45,35 @@ task spi_shift;
     dshift <= sval;
     start  <= 1;
 
-    @(posedge busy);
+    @(posedge busy); // pretend this is slave select
     start  <= 0;
-
-    @(posedge mclk);
+    din    <= 8'hxx;
     miso   <= dshift[7];
-    @(negedge mclk);
-    dshift <= {dshift[6:0], mosi};
 
-    @(posedge mclk);
-    miso   <= dshift[7];
-    @(negedge mclk);
-    dshift <= {dshift[6:0], mosi};
+    for(i=0; i<8; i=i+1) begin
+      // phase 0
+      if(cpol==0)
+        @(posedge mclk);
+      else
+        @(negedge mclk);
+      // phase 1
+      if(cpha==0)
+        dshift <= {dshift[6:0], mosi};
+      else
+        miso   <= dshift[7];
+      // phase 2
+      if(cpol==0)
+        @(negedge mclk);
+      else
+        @(posedge mclk);
+      // phase 3
+      if(cpha==0)
+        miso   <= dshift[7];
+      else
+        dshift <= {dshift[6:0], mosi};
+    end
 
-    @(posedge mclk);
-    miso   <= dshift[7];
-    @(negedge mclk);
-    dshift <= {dshift[6:0], mosi};
-
-    @(posedge mclk);
-    miso   <= dshift[7];
-    @(negedge mclk);
-    dshift <= {dshift[6:0], mosi};
-
-    @(posedge mclk);
-    miso   <= dshift[7];
-    @(negedge mclk);
-    dshift <= {dshift[6:0], mosi};
-
-    @(posedge mclk);
-    miso   <= dshift[7];
-    @(negedge mclk);
-    dshift <= {dshift[6:0], mosi};
-
-    @(posedge mclk);
-    miso   <= dshift[7];
-    @(negedge mclk);
-    dshift <= {dshift[6:0], mosi};
-
-    @(posedge mclk);
-    miso   <= dshift[7];
-    @(negedge mclk);
-    dshift <= {dshift[6:0], mosi};
+    miso   <= 1'bz;
 
     `ASSERT_EQUAL(0, busy)
 
@@ -95,7 +87,31 @@ initial
 begin
   `TEST_INIT(test)
 
+  cpol = 0;
+  cpha = 0;
   #6
+  $display("config CPOL=%d CPHA=%d", cpol, cpha);
+  spi_shift(8'ha1, 8'hb2);
+  spi_shift(8'h51, 8'h62);
+
+  cpol = 1;
+  cpha = 0;
+  #6
+  $display("config CPOL=%d CPHA=%d", cpol, cpha);
+  spi_shift(8'ha1, 8'hb2);
+  spi_shift(8'h51, 8'h62);
+
+  cpol = 0;
+  cpha = 1;
+  #6
+  $display("config CPOL=%d CPHA=%d", cpol, cpha);
+  spi_shift(8'ha1, 8'hb2);
+  spi_shift(8'h51, 8'h62);
+
+  cpol = 1;
+  cpha = 1;
+  #6
+  $display("config CPOL=%d CPHA=%d", cpol, cpha);
   spi_shift(8'ha1, 8'hb2);
   spi_shift(8'h51, 8'h62);
 
