9edc1d6252522c6449536aa2c313f52d7a099530
Michael Davidsaver <mdavidsaver@gmail.com>
2016-10-07 19:43:34 -0400

    add sync fifo

---
+++ util/sfifo.v
@@ -0,0 +1,80 @@
+/* Synchronous FIFO
+ * Depth is a power of 2 (1<<DEPTH)
+ * sync reset must be held high until both wclk and rclk tick once
+ * Most flow control signals available on both sides.
+ *
+ * wpos and rpos are the next memory elements to be written/read respectively.
+ */
+module sfifo(
+  input               reset,
+  input               clk,
+  // Writer side
+  input   [WIDTH-1:0] wdata,  // must be valid while wstore is high
+  input               store, // push while high
+  // Reader side
+  input               read, // pop while high
+  output  [WIDTH-1:0] rdata, // valid when ~rempty
+  // Status
+  output              empty,
+  output              full,
+  output              overflow
+);
+
+//reg [WIDTH-1:0] rdata;
+
+// "REPLACE" overwrite last value pushed with new value being pushed
+// "IGNORE"  ignore new value being pushed
+parameter OFLOW = "REPLACE";
+
+// # of data bits
+parameter WIDTH=8;
+
+// # of address bits in fifo
+parameter DEPTH=4;
+// # number of memory elements.  Can actually store NELEM-1
+localparam NELEM = 1<<DEPTH;
+
+
+reg [WIDTH-1:0] buffer [NELEM-1:0];
+
+reg  [DEPTH-1:0] rpos=0; // read pointer
+reg  [DEPTH-1:0] wpos=0; // write pointer
+wire [DEPTH-1:0] wpos_next = wpos+1;
+
+assign empty = rpos==wpos;
+assign full  = rpos==wpos_next;
+assign overflow = store & full;
+
+//always @(posedge clk)
+//  if(read)
+//    rdata <= buffer[rpos];
+assign rdata = buffer[rpos];
+
+always @(posedge clk)
+  if(reset)
+    rpos <= 0;
+  else if(read & ~empty)
+    rpos <= rpos+1;
+
+wire dostore;
+generate
+  if(OFLOW=="REPLACE")
+    assign dostore = store;
+  else if(OFLOW=="IGNORE")
+    assign dostore = store & ~full;
+  else
+    OFLOW_value_not_valid foo();
+endgenerate
+
+always @(posedge clk)
+  if(dostore)
+    buffer[wpos] <= wdata;
+
+always @(posedge clk)
+  if(reset) begin
+    wpos         <= 0;
+  end else if(store & ~full) begin
+    wpos         <= wpos_next;
+  end
+
+endmodule
---
+++ util/sfifo_tb.v
@@ -0,0 +1,169 @@
+module test;
+
+`include "utest.vlib"
+
+`TEST_PRELUDE(52)
+
+`TEST_CLOCK(clk,10);
+
+`TEST_TIMEOUT(200000)
+
+reg reset = 1, store=0, read=0;
+
+reg [7:0] wdata;
+
+sfifo #(
+  .WIDTH(8),
+  .DEPTH(2)  // 4 entries
+) dut (
+  .reset(reset),
+  .clk(clk),
+  .store(store),
+  .read(read),
+  .wdata(wdata)
+);
+
+task push;
+  input [7:0] val;
+  begin
+    $display("# push %02x", val);
+    while(~clk) @(posedge clk);
+    store <= 1;
+    wdata <= val;
+    @(posedge clk);
+    store <= 0;
+    wdata <= 8'hxx;
+  end
+endtask
+
+task pop;
+  input [7:0] expect;
+  begin
+    $display("# pop %02x", expect);
+    while(~clk) @(posedge clk);
+    read <= 1;
+    @(posedge clk);
+    read <= 0;
+    `ASSERT_EQUAL(dut.rdata, expect, "read elem")
+  end
+endtask
+
+task pushpop;
+  input [7:0] val;
+  input [7:0] expect;
+  begin
+    $display("# push %02x, pop %02x", val, expect);
+    while(~clk) @(posedge clk);
+    store <= 1;
+    wdata <= val;
+    read <= 1;
+    @(posedge clk);
+    store <= 0;
+    wdata <= 8'hxx;
+    read <= 0;
+    `ASSERT_EQUAL(dut.rdata, expect, "read elem")
+  end
+endtask
+
+initial
+begin
+  `TEST_INIT(test)
+
+  @(posedge clk);
+  @(posedge clk);
+  reset <= 0;
+  @(posedge clk);
+  @(posedge clk);
+
+  $display("# T %d", $simtime);
+  `ASSERT_EQUAL(dut.empty, 1, "empty")
+  `ASSERT_EQUAL(dut.full,  0, "full")
+  `ASSERT_EQUAL(dut.rpos, 0, "rpos")
+  `ASSERT_EQUAL(dut.wpos, 0, "wpos")
+
+  push(8'h12);
+
+  @(posedge clk);
+  $display("# T %d", $simtime);
+  `ASSERT_EQUAL(dut.empty, 0, "empty")
+  `ASSERT_EQUAL(dut.full,  0, "full")
+  `ASSERT_EQUAL(dut.rpos, 0, "rpos")
+  `ASSERT_EQUAL(dut.wpos, 1, "wpos")
+  `ASSERT_EQUAL(dut.buffer[0], 8'h12, "buffer[0]")
+
+  push(8'h34);
+  push(8'h56);
+
+  @(posedge clk);
+  $display("# T %d", $simtime);
+  `ASSERT_EQUAL(dut.empty, 0, "empty")
+  `ASSERT_EQUAL(dut.full,  1, "full")
+  `ASSERT_EQUAL(dut.rpos, 0, "rpos")
+  `ASSERT_EQUAL(dut.wpos, 3, "wpos")
+  `ASSERT_EQUAL(dut.buffer[0], 8'h12, "buffer[0]")
+  `ASSERT_EQUAL(dut.buffer[1], 8'h34, "buffer[1]")
+  `ASSERT_EQUAL(dut.buffer[2], 8'h56, "buffer[2]")
+
+  pop(8'h12);
+  pop(8'h34);
+  pop(8'h56);
+
+  @(posedge clk);
+  $display("# T %d", $simtime);
+  `ASSERT_EQUAL(dut.empty, 1, "empty")
+  `ASSERT_EQUAL(dut.full,  0, "full")
+  `ASSERT_EQUAL(dut.rpos, 3, "rpos")
+  `ASSERT_EQUAL(dut.wpos, 3, "wpos")
+  `ASSERT_EQUAL(dut.buffer[0], 8'h12, "buffer[0]")
+  `ASSERT_EQUAL(dut.buffer[1], 8'h34, "buffer[1]")
+  `ASSERT_EQUAL(dut.buffer[2], 8'h56, "buffer[2]")
+
+  push(8'h78);
+  pushpop(8'h9a, 8'h78);
+
+  @(posedge clk);
+  $display("# T %d", $simtime);
+  `ASSERT_EQUAL(dut.empty, 0, "empty")
+  `ASSERT_EQUAL(dut.full,  0, "full")
+  `ASSERT_EQUAL(dut.rpos, 0, "rpos")
+  `ASSERT_EQUAL(dut.wpos, 1, "wpos")
+  `ASSERT_EQUAL(dut.buffer[0], 8'h9a, "buffer[0]")
+  `ASSERT_EQUAL(dut.buffer[1], 8'h34, "buffer[1]")
+  `ASSERT_EQUAL(dut.buffer[2], 8'h56, "buffer[2]")
+  `ASSERT_EQUAL(dut.buffer[3], 8'h78, "buffer[3]")
+
+  pop(8'h9a);
+  push(8'hbc);
+  push(8'hde);
+  push(8'hf0);
+
+  @(posedge clk);
+  $display("# T %d", $simtime);
+  `ASSERT_EQUAL(dut.empty, 0, "empty")
+  `ASSERT_EQUAL(dut.full,  1, "full")
+  `ASSERT_EQUAL(dut.rpos, 1, "rpos")
+  `ASSERT_EQUAL(dut.wpos, 0, "wpos")
+  `ASSERT_EQUAL(dut.buffer[0], 8'h9a, "buffer[0]")
+  `ASSERT_EQUAL(dut.buffer[1], 8'hbc, "buffer[1]")
+  `ASSERT_EQUAL(dut.buffer[2], 8'hde, "buffer[2]")
+  `ASSERT_EQUAL(dut.buffer[3], 8'hf0, "buffer[3]")
+
+  reset <= 1;
+  @(posedge clk);
+  reset <= 0;
+  
+  @(posedge clk);
+  $display("# T %d", $simtime);
+  `ASSERT_EQUAL(dut.empty, 1, "empty")
+  `ASSERT_EQUAL(dut.full,  0, "full")
+  `ASSERT_EQUAL(dut.rpos, 0, "rpos")
+  `ASSERT_EQUAL(dut.wpos, 0, "wpos")
+  `ASSERT_EQUAL(dut.buffer[0], 8'h9a, "buffer[0]")
+  `ASSERT_EQUAL(dut.buffer[1], 8'hbc, "buffer[1]")
+  `ASSERT_EQUAL(dut.buffer[2], 8'hde, "buffer[2]")
+  `ASSERT_EQUAL(dut.buffer[3], 8'hf0, "buffer[3]")
+
+  `TEST_DONE
+end
+
+endmodule
