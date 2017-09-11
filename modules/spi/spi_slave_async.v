cdf6b29aac468d0dd04021388143355b69e1e572
Michael Davidsaver <mdavidsaver@gmail.com>
2017-09-11 11:14:43 -0500

    async SPI slave

---
+++ modules/spi/spi_slave_async.v
@@ -0,0 +1,89 @@
+/* SPI slave w/o local clock input
+ *
+ * setup of rising edge of sclk.
+ * samples on falling edge of sclk.
+ * active high slave select (ss)
+ *
+ * User logic should act on rising edge of 'clk'
+ * to use 'mdat' to drive 'sdat'.
+ */
+module spi_slave_async(
+    input ss,   // active high
+    input sclk,
+    input mosi,
+    output miso,
+
+    output clk, // gated clock
+    output ready,
+    output [0:7] mdat, // master data
+    input [0:7] sdat, // slave data
+    output reset
+);
+
+wire reset = ~ss;
+
+wire clk = ss & ~sclk;
+
+reg mosi_l;
+always @(negedge sclk)
+  mosi_l <= mosi;
+
+reg ready;
+reg [0:2] state;
+
+always @(posedge sclk, negedge ss)
+  if(!ss)
+    {ready, state} <= 4'h00;
+  else
+    {ready, state} <= state + 1;
+
+reg miso;
+always @(posedge sclk)
+  if(ready)
+    miso <= sdat[0];
+  else
+    miso <= dshift[1];
+
+// sampled when mosi is stable, posedge clk (aka negedge sclk)
+wire [0:7] mdat = {dshift[1:7], mosi};
+
+reg [0:7] dshift;
+
+always @(posedge sclk)
+  if(ready)
+    dshift <= sdat;
+  else
+    dshift <= {dshift[1:7], mosi_l};
+
+
+always @(posedge sclk, negedge ss)
+  $display("/ %d ss=%d state=%d ready=%d", $simtime, ss, state, ready);
+always @(negedge sclk)
+  $display("\\ %d sdat=%x dshift=%x mosi=%d", $simtime, sdat, dshift, mosi);
+endmodule
+
+/*
+http://wavedrom.com/editor.html
+
+
+{signal: [
+  {name: 'sclk', wave: '0..10101010101010101010101010101010..'},
+  {name: 'ss',   wave: '01..................................0'},
+  {name: 'mosi', wave: 'xxx=.=.=.=.=.=.=.=.xxxxxxxxxxxxxxxxxx', data: ['M7', 'M6', 'M5', 'M4', 'M3', 'M2', 'M1', 'M0']},
+  {name: 'miso', wave: 'xxxxxxxxxxxxxxxxxxx=.=.=.=.=.=.=.=.xx', data: ['S7', 'S6', 'S5', 'S4', 'S3', 'S2', 'S1', 'S0']},
+  {},
+  {name: 'state',wave: '=..=.=.=.=.=.=.=.=.=.=.=.=.=.=.=.=...', data:['0','1','2','3','4','5','6','7','0','1','2','3','4','5','6','7','0']},
+  {name:'ready', wave: '0................1.0.'},
+  {name:'sample',wave: '0.................1.0'},
+  {name:'mdat' , wave: 'xxxxxxxxxxxxxxxxxx=.xxxxxxxxxxxxxxxxx'},
+  {name:'sdat' , wave: 'xxxxxxxxxxxxxxxxxxx=.xxxxxxxxxxxxxxxx'},
+  {name:'dshift[7]', wave: 'xxxxxxxxxxxxxxxxxx=.=.=.=.=.=.=.=.xxx', data: ['S7', 'S6', 'S5', 'S4', 'S3', 'S2', 'S1', 'S0']},
+  {name:'dshift[6]', wave: 'xxxxxxxxxxxxxxxx=.=.=.=.=.=.=.=.xxxxx', data: ['M7', 'S6', 'S5', 'S4', 'S3', 'S2', 'S1', 'S0']},
+  {name:'dshift[5]', wave: 'xxxxxxxxxxxxxx=.=.=.=.=.=.=.=.xxxxxxx', data: ['M7', 'M6', 'S5', 'S4', 'S3', 'S2', 'S1', 'S0']},
+  {name:'dshift[4]', wave: 'xxxxxxxxxxxx=.=.=.=.=.=.=.=.xxxxxxxxx', data: ['M7', 'M6', 'M5', 'S4', 'S3', 'S2', 'S1', 'S0']},
+  {name:'dshift[3]', wave: 'xxxxxxxxxx=.=.=.=.=.=.=.=.xxxxxxxxxxx', data: ['M7', 'M6', 'M5', 'M4', 'S3', 'S2', 'S1', 'S0']},
+  {name:'dshift[2]', wave: 'xxxxxxxx=.=.=.=.=.=.=.=.xxxxxxxxxxxxx', data: ['M7', 'M6', 'M5', 'M4', 'M3', 'S2', 'S1', 'S0']},
+  {name:'dshift[1]', wave: 'xxxxxx=.=.=.=.=.=.=.=.xxxxxxxxxxxxxxx', data: ['M7', 'M6', 'M5', 'M4', 'M3', 'M2', 'S1', 'S0']},
+  {name:'dshift[0]', wave: 'xxxx=.=.=.=.=.=.=.=.xxxxxxxxxxxxxxxxx', data: ['M7', 'M6', 'M5', 'M4', 'M3', 'M2', 'M1', 'S0']},
+]}
+*/
---
+++ modules/spi/spi_slave_async_tb.v
@@ -0,0 +1,108 @@
+module test;
+
+`include "utest.vlib"
+
+`TEST_PRELUDE(7)
+
+`TEST_TIMEOUT(6000)
+
+reg sclk, mosi, ss;
+wire miso;
+
+reg [0:7] outdat;
+reg [0:7] indat;
+
+reg [0:7] tsdat;
+reg [0:7] tmdat;
+
+task spi_xfer;
+  input [0:7] in;
+  output [0:7] out;
+  begin
+    $display("spi_xfer start in=%x", in);
+    #2 mosi <= in[0];
+    sclk <= 1;
+    #2 sclk <= 0;
+    out[0] <= miso;
+
+    #2 mosi <= in[1];
+    sclk <= 1;
+    #2 sclk <= 0;
+    out[1] <= miso;
+
+    #2 mosi <= in[2];
+    sclk <= 1;
+    #2 sclk <= 0;
+    out[2] <= miso;
+
+    #2 mosi <= in[3];
+    sclk <= 1;
+    #2 sclk <= 0;
+    out[3] <= miso;
+
+    #2 mosi <= in[4];
+    sclk <= 1;
+    #2 sclk <= 0;
+    out[4] <= miso;
+
+    #2 mosi <= in[5];
+    sclk <= 1;
+    #2 sclk <= 0;
+    out[5] <= miso;
+
+    #2 mosi <= in[6];
+    sclk <= 1;
+    #2 sclk <= 0;
+    out[6] <= miso;
+
+    #2 mosi <= in[7];
+    sclk <= 1;
+    #2 sclk <= 0;
+    out[7] = miso; // immediate assignment
+    `ASSERT_EQUAL(dut.ready, 1, "ready")
+  end
+endtask
+
+reg [0:7] sdatl;
+
+spi_slave_async dut(
+    .sclk(sclk),
+    .miso(miso),
+    .mosi(mosi),
+    .ss(ss),
+    .sdat(sdatl)
+);
+
+always @(posedge dut.clk)
+  if(dut.ready) begin
+    $display("@ %d Ready mdat=%x sdat=%x", $simtime, dut.mdat, tsdat);
+    sdatl <= tsdat;
+    tmdat <= dut.mdat;
+  end else
+    sdatl <= 8'hxx;
+
+initial
+begin
+  `TEST_INIT(test)
+
+  sclk <= 0;
+  mosi <= 0;
+  ss <= 0;
+  #2
+  ss <= 1;
+
+  spi_xfer(8'b01101010, outdat);
+  `ASSERT_EQUAL(dut.mdat, 8'b01101010, "master data")
+  tsdat <= 8'b10010101;
+  spi_xfer(8'b10010001, outdat);
+  `ASSERT_EQUAL(dut.mdat, 8'b10010001, "master data")
+  `ASSERT_EQUAL(outdat, 8'b10010101, "slave data")
+  tsdat <= 8'b10010010;
+  spi_xfer(8'hxx, outdat);
+  `ASSERT_EQUAL(outdat, 8'b10010010, "slave data")
+  tsdat <= 8'hxx;
+
+  #8 `TEST_DONE
+end
+
+endmodule
